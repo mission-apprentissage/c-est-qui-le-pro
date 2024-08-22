7671c51d00f1958e4a76f9a2b5a0c935c44cf18d
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-08-22 04:15:32 +0200

    Removing tss-react dependency

--- ui/app/components/Header.tsx
+++ ui/app/components/Header.tsx
@@ -1,7 +1,8 @@
 "use client";
 import Header from "@codegouvfr/react-dsfr/Header";
 import { Badge } from "@codegouvfr/react-dsfr/Badge";
-import { makeStyles } from "tss-react/dsfr";
+import { css } from "@emotion/css";
+import { fr } from "@codegouvfr/react-dsfr";
 
 export default function CustomHeader({
   title,
@@ -14,11 +15,20 @@ export default function CustomHeader({
   quickAccessItems?: JSX.Element[];
   withBetaTag?: boolean;
 }) {
-  const { classes, cx } = useStyles();
 
   return (
     <Header
-      className={cx(classes.root)}
+      className={css`
+        ul {
+          flex-grow: 1;
+          li {
+            flex-grow: 1;
+          }
+        }
+        .${fr.cx("fr-header__tools")} {
+          flex: 1 1 auto;
+        }
+      `}
       brandTop={
         <>
           RÉPUBLIQUE
@@ -50,17 +60,3 @@ export default function CustomHeader({
 }
 
 CustomHeader.displayName = CustomHeader.name;
-
-const useStyles = makeStyles({ name: CustomHeader.name })((theme) => ({
-  root: {
-    ul: {
-      flexGrow: "1",
-      li: {
-        flexGrow: 1,
-      },
-    },
-    ".fr-header__tools": {
-      flex: "1 1 auto",
-    },
-  },
-}));
