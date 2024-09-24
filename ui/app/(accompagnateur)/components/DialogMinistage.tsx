faa5be6cd6c77b83e86cf7def671778cc16a4b42
Ananda <ananda10@gmail.com>
2024-09-24 14:01:58 +0200

    style: wording des dialogs

--- ui/app/(accompagnateur)/components/DialogMinistage.tsx
+++ ui/app/(accompagnateur)/components/DialogMinistage.tsx
@@ -21,6 +21,7 @@ export default function DialogMinistage() {
         <p style={{ marginBottom: fr.spacing("8v") }}>
           Le mini-stage est une <b>session d’immersion</b> destinée aux élèves de 3ème et de 2nde professionnelle, d’une
           durée généralement comprise <b>entre une demi-journée et 2 jours</b>, dans une classe de la formation choisie.
+          Les mini-stages se déroulent habituellement entre Décembre et Avril.
           <br />
           <br />
           Le stagiaire suit les cours, théoriques ou pratiques, de la formation professionnelle. Il peut ainsi
@@ -34,8 +35,8 @@ export default function DialogMinistage() {
           Comment s’inscrire ?
         </Typography>
         <p style={{ marginBottom: fr.spacing("3v") }}>
-          L’inscription se fait exclusivement via le collège dans lequel est scolarisé l’élève. Il doit donc commencer
-          par en parler avec son professeur principal, CPE ou directeur d’établissement.
+          L’inscription se fait au secrétariat du collège dans lequel est scolarisé l’élève. Il doit donc commencer par
+          en parler avec son professeur principal, CPE ou chef d’établissement.
         </p>
       </modalMinistage.Component>
     </>
