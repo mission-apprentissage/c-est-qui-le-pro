784a49a63b155a28c67e6cae377b4b32513fd23e
Ananda <ananda10@gmail.com>
2024-10-21 11:16:07 +0100

    fix: seuil dans les indicateurs poursuites

--- server/src/services/exposition/utils.ts
+++ server/src/services/exposition/utils.ts
@@ -1,7 +1,12 @@
 import { isNil, round } from "lodash-es";
 
 export function computeTauxEnEmploi(stats) {
-  if (isNil(stats.nb_sortant) || isNil(stats.nb_en_emploi_6_mois) || stats.nb_sortant === 0) {
+  if (
+    isNil(stats.taux_en_emploi_6_mois) || // Seuil
+    isNil(stats.nb_sortant) ||
+    isNil(stats.nb_en_emploi_6_mois) ||
+    stats.nb_sortant === 0
+  ) {
     return null;
   }
 
