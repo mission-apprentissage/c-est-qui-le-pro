2a33f353ec4a45c8a3c497217933f5ca0e723010
Ananda <ananda10@gmail.com>
2025-02-27 17:43:27 +0100

    style: fix wording

--- ui/app/(accompagnateur)/details/[id]/FormationResumeBlockEtude.tsx
+++ ui/app/(accompagnateur)/details/[id]/FormationResumeBlockEtude.tsx
@@ -65,7 +65,7 @@ function FormationResumeBlockEtudeAnneeCommuneTag({
     <>
       {hasStatTerminale ? (
         <Tag square variant="dark-blue" bold={"500"}>
-          Différente selon le Bac Pro choisi après de cette seconde commune
+          Différente selon le Bac Pro choisi après cette seconde commune
         </Tag>
       ) : (
         <Tag square level="unknow">
