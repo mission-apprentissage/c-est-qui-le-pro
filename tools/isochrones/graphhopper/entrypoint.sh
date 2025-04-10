d2e712efb73890eb44ca43b606f0b8b25b6e2646
Ananda <ananda10@gmail.com>
2025-04-10 11:26:35 +0200

    feat: helm chart pour la génération des isochrones

--- tools/isochrones/graphhopper/entrypoint.sh
+++ tools/isochrones/graphhopper/entrypoint.sh
@@ -1,4 +1,40 @@
 #!/bin/sh
 set -e
 
+mkdir -p /home/onyxia/work/
+
+if [ -d /home/onyxia/work/pbf ]; then
+    echo "PBF files already exist"
+else
+    # Download pbf
+    mkdir -p /home/onyxia/work/pbf
+    cd /home/onyxia/work/pbf
+
+    wget --progress=dot:giga https://download.geofabrik.de/europe/france-latest.osm.pbf
+    wget --progress=dot:giga https://download.geofabrik.de/europe/france/martinique-latest.osm.pbf
+    wget --progress=dot:giga https://download.geofabrik.de/europe/france/guadeloupe-latest.osm.pbf
+    wget --progress=dot:giga https://download.geofabrik.de/europe/france/mayotte-latest.osm.pbf
+    wget --progress=dot:giga https://download.geofabrik.de/europe/france/guyane-latest.osm.pbf
+    wget --progress=dot:giga https://download.geofabrik.de/europe/france/reunion-latest.osm.pbf
+
+    # Merge pbf
+    echo "Merging PBF files"
+    osmium merge france-latest.osm.pbf martinique-latest.osm.pbf guadeloupe-latest.osm.pbf mayotte-latest.osm.pbf guyane-latest.osm.pbf reunion-latest.osm.pbf -o full.osm.pbf
+fi
+
+if [ -d /home/onyxia/work/gtfs ]; then
+    echo "GTFS files already exist"
+else
+    # Download and clean gtfs
+    mkdir -p /home/onyxia/work/gtfs
+    cd /home/onyxia/work/gtfs
+    bash /scripts/gtfs.sh
+fi
+
+cp /scripts/config.yml /home/onyxia/work/
+cp /graphhopper/graphhopper-web-9.1.jar /home/onyxia/work/
+cd /home/onyxia/work/
+
+echo "Starting Graphhopper"
+
 java -Xmx$GRAPHHOPPER_MEMORY_LIMIT -Xms$GRAPHHOPPER_MEMORY_LIMIT -jar graphhopper-web-9.1.jar server config.yml
--- tools/isochrones/graphhopper/gtfs.sh
+++ tools/isochrones/graphhopper/gtfs.sh
@@ -2,16 +2,147 @@
 set -e
 
 function download_and_clean() {
-    wget $*
-    filename=$(basename "$*")
-    $(go env GOPATH)/bin/gtfsclean -SCRmTcdsOeD $filename
+    local url="$1"
+    local filename="$2"
+    local zip_folder="$3"
+
+    wget --progress=dot:giga -O "$filename" "$url"
+
+    # Some GTFS zip has a subfolder
+    if [[ "$zip_folder" == "1" ]]; then
+        mkdir tmp
+        unzip "$filename" -d tmp
+        cd tmp && cd "$(ls -d */ | head -1 | tr -d '/')"
+        zip -0 "$filename" *
+        mv "$filename" ../..
+        cd ../..
+        rm -Rf tmp
+    fi
+
+    $(go env GOPATH)/bin/gtfsclean -SCRmTcdsOeD "$filename"
     cd gtfs-out
-    zip -0 $filename *
-    mv $filename ..
+    zip -0 "$filename" *
+    mv "$filename" ..
     cd ..
     rm -Rf gtfs-out
 }
 
-download_and_clean https://eu.ftp.opendatasoft.com/stif/GTFS/IDFM-gtfs.zip
-download_and_clean https://www.korrigo.bzh/ftp/OPENDATA/KORRIGOBRET.gtfs.zip
-download_and_clean https://eu.ftp.opendatasoft.com/sncf/plandata/export-ter-gtfs-last.zip
+echo "Downloading GTFS files"
+
+# 01 Guadeloupe
+# Pas de source
+
+# 02 Martinique
+download_and_clean "https://www.data.gouv.fr/fr/datasets/r/d910480b-c9a5-4f48-a257-4e1aa799c5c8" "martinique-sud.zip"
+download_and_clean "https://www.data.gouv.fr/fr/datasets/r/82481c27-2e52-40ef-a563-b011ba487ead" "martinique-nord.zip" 1
+download_and_clean "https://www.data.gouv.fr/fr/datasets/r/6e599077-0719-44b4-82ad-0da90a282846" "martinique-centre.zip"
+
+# 03 Guyane
+# Pas de source
+
+# 04 La Réunion
+download_and_clean "https://pysae.com/api/v2/groups/car-jaune/gtfs/pub" "reunion-p1.zip"
+download_and_clean "https://www.data.gouv.fr/fr/datasets/r/fc065c47-8644-4941-a8ca-4d8322a45749" "reunion-p2.zip"
+download_and_clean "https://www.data.gouv.fr/fr/datasets/r/919b4ca6-11e3-4156-bf59-5c0e7f25d929" "reunion-p3.zip"
+download_and_clean "https://www.data.gouv.fr/fr/datasets/r/8f3642e3-9fc3-45ed-af46-8c532966ace3" "reunion-p4.zip"
+download_and_clean "https://www.data.gouv.fr/fr/datasets/r/c9c2f609-d0cd-4233-ad1b-cf86b9bf2dc8" "reunion-p5.zip"
+download_and_clean "https://www.data.gouv.fr/fr/datasets/r/6fee690a-f80c-4083-ac19-34341b864fe8" "reunion-p6.zip"
+
+# 05 Mayotte
+# Pas de source
+
+# 11 Ile de france
+download_and_clean "https://eu.ftp.opendatasoft.com/stif/GTFS/IDFM-gtfs.zip" "ile-de-france.zip"
+
+# 24 Centre-Val de Loire
+download_and_clean "https://data.centrevaldeloire.fr/api/explore/v2.1/catalog/datasets/jvmalin-point-dacces-national/files/e4c49416b6a28d91287b6c81b8ba0569" "centre-val-de-loire.zip"
+
+# 27 Bourgogne-Franche-Comté
+# Doubs
+download_and_clean "https://exs.mobigo.cityway.fr/gtfs.aspx?key=OPENDATA&operatorCode=UT25" "bfc-doubs.zip"
+# Côte d'Or
+download_and_clean "https://exs.mobigo.cityway.fr/gtfs.aspx?key=OPENDATA&operatorCode=UT21" "bfc-cote-dor.zip"
+# Nièvre
+download_and_clean "https://exs.mobigo.cityway.fr/gtfs.aspx?key=OPENDATA&operatorCode=UT58" "bfc-nievre.zip"
+# Haute-Saône
+download_and_clean "https://exs.mobigo.cityway.fr/gtfs.aspx?key=OPENDATA&operatorCode=UT70" "bfc-haute-saone.zip"
+# Saône et Loire
+download_and_clean "https://exs.mobigo.cityway.fr/gtfs.aspx?key=OPENDATA&operatorCode=UT71" "bfc-saone-et-loire.zip"
+# Jura
+download_and_clean "https://exs.mobigo.cityway.fr/gtfs.aspx?key=OPENDATA&operatorCode=UT39" "bfc-jura.zip"
+# Yonne
+download_and_clean "https://exs.mobigo.cityway.fr/gtfs.aspx?key=OPENDATA&operatorCode=UT89" "bfc-yonne.zip"
+
+# 28 Normandie
+download_and_clean "https://www.data.gouv.fr/fr/datasets/r/3873ccc7-44d9-40f3-a133-920944af0c29" "normandie.zip"
+
+# 32 Hauts-de-France
+# Aisne
+download_and_clean "https://geocatalogue.hautsdefrance.fr/gtfs/RHDF_GTFS_COM_02.zip" "hdf-aisne.zip"
+# Somme
+download_and_clean "https://sig.hautsdefrance.fr/ext/opendata/Transport/GTFS/80/RHDF_GTFS_COM_80.zip" "hdf-somme.zip"
+# Pas-de-Calais
+download_and_clean "https://geocatalogue.hautsdefrance.fr/gtfs/RHDF_GTFS_COM_62.zip" "hdf-pas-de-calais.zip"
+# Oise
+download_and_clean "https://geocatalogue.hautsdefrance.fr/gtfs/RHDF_GTFS_COM_SCO_60_P1.zip" "hdf-oise-p1.zip"
+download_and_clean "https://geocatalogue.hautsdefrance.fr/gtfs/RHDF_GTFS_COM_SCO_60_P2.zip" "hdf-oise-p2.zip"
+download_and_clean "https://geocatalogue.hautsdefrance.fr/gtfs/RHDF_GTFS_COM_SCO_60_P3.zip" "hdf-oise-p3.zip"
+download_and_clean "https://geocatalogue.hautsdefrance.fr/gtfs/RHDF_GTFS_COM_SCO_60_P4.zip" "hdf-oise-p4.zip"
+# Nord
+download_and_clean "https://geocatalogue.hautsdefrance.fr/gtfs/RHDF_GTFS_COM_SCO_59_P1.zip" "hdf-nord-p1.zip"
+# Le dernier fichier est corrompu, utilisation d'une archive
+# download_and_clean "https://geocatalogue.hautsdefrance.fr/gtfs/RHDF_GTFS_COM_SCO_59_P2.zip" "hdf-nord-p2.zip"
+download_and_clean "https://transport-data-gouv-fr-resource-history-prod.cellar-c2.services.clever-cloud.com/81829/81829.20250405.000930.740393.zip" "hdf-nord-p2.zip"
+download_and_clean "https://geocatalogue.hautsdefrance.fr/gtfs/RHDF_GTFS_COM_SCO_59_P3A.zip" "hdf-nord-p3a.zip"
+download_and_clean "https://geocatalogue.hautsdefrance.fr/gtfs/RHDF_GTFS_COM_SCO_59_P3B.zip" "hdf-nord-p3b.zip"
+download_and_clean "https://geocatalogue.hautsdefrance.fr/gtfs/RHDF_GTFS_COM_SCO_59_P4.zip" "hdf-nord-p4.zip"
+
+# 44 Grand Est
+# Ardennes
+download_and_clean "https://www.datagrandest.fr/metadata/fluo-grand-est/FR-200052264-T0045-0000/fluo-grand-est-fluo08-gtfs.zip" "ge-ardennes.zip"
+# Aube
+download_and_clean "https://www.datagrandest.fr/metadata/fluo-grand-est/FR-200052264-T0046-0000/fluo-grand-est-fluo10-gtfs.zip" "ge-aube.zip"
+# Bas-Rhin
+download_and_clean "https://www.datagrandest.fr/metadata/fluo-grand-est/FR-200052264-T0049-0000/fluo-grand-est-fluo67-gtfs.zip" "ge-bas-rhin.zip"
+# Haute-Marne
+download_and_clean "https://www.datagrandest.fr/metadata/fluo-grand-est/FR-200052264-T0055-0000/fluo-grand-est-fluo52-gtfs.zip" "ge-haute-marne.zip"
+# Haut-Rhin
+download_and_clean "https://www.datagrandest.fr/metadata/fluo-grand-est/FR-200052264-T0050-0000/fluo-grand-est-fluo68-gtfs.zip" "ge-haut-rhin.zip"
+# Marne
+download_and_clean "https://www.datagrandest.fr/metadata/fluo-grand-est/FR-200052264-T0043-0000/fluo-grand-est-fluo51-gtfs.zip" "ge-marne.zip"
+# Meurthe-et-Moselle
+download_and_clean "https://www.datagrandest.fr/metadata/fluo-grand-est/FR-200052264-T0051-0000/fluo-grand-est-fluo54-gtfs.zip" "ge-meurthe.zip"
+# Meuse
+download_and_clean "https://www.datagrandest.fr/metadata/fluo-grand-est/FR-200052264-T0048-0000/fluo-grand-est-fluo55-gtfs.zip" "ge-meuse.zip"
+# Moselle
+download_and_clean "https://www.datagrandest.fr/metadata/fluo-grand-est/FR-200052264-T0052-0000/fluo-grand-est-fluo57-gtfs.zip" "ge-moselle.zip"
+# Vosges
+download_and_clean "https://www.datagrandest.fr/metadata/fluo-grand-est/FR-200052264-T0042-0000/fluo-grand-est-fluo88-gtfs.zip" "ge-vosges.zip"
+
+# 52 Pays de la Loire
+download_and_clean "https://mobi-iti-pdl.okina.fr/static/mobiiti_technique/gtfs_global.zip" "pays-de-la-loire.zip"
+
+# 53 Bretagne
+download_and_clean "https://www.korrigo.bzh/ftp/OPENDATA/KORRIGOBRET.gtfs.zip" "bretagne.zip"
+
+# 75 Nouvelle-Aquitaine
+download_and_clean "https://www.pigma.org/public/opendata/nouvelle_aquitaine_mobilites/publication/naq-aggregated-gtfs.zip" "nouvelle-aquitaine.zip"
+
+# 76 Occitanie
+download_and_clean "https://app.mecatran.com/utw/ws/gtfsfeed/static/lio?apiKey=2b160d626f783808095373766f18714901325e45&type=gtfs_lio" "occitanie.zip"
+
+# 84 Auvergne-Rhône-Alpes
+download_and_clean "https://mobi-iti-ara.okina.fr/static/mobiiti_technique/DAT_AURA_GTFS_ExportAOM.zip" "auvergne.zip"
+
+# 93 Provence-Alpes-Côte d'Azur
+download_and_clean "https://www.datasud.fr/fr/dataset/datasets/3745/resource/5016/download/" "provence-p1.zip"
+download_and_clean "https://www.datasud.fr/fr/dataset/datasets/3743/resource/5153/download/" "provence-p2.zip"
+
+# 94 Corse
+# Haute Corse
+download_and_clean "https://www.data.gouv.fr/fr/datasets/r/f6fe5d5a-8ad7-4de2-a1b1-ea1f5eacd16a" "corse-haute.zip"
+# Corse du Sud
+download_and_clean "https://www.data.gouv.fr/fr/datasets/r/416261b8-016d-4639-bab5-39a588665fc7" "corse-sud.zip"
+
+# TER national
+download_and_clean "https://eu.ftp.opendatasoft.com/sncf/plandata/export-ter-gtfs-last.zip" "ter-national.zip"
