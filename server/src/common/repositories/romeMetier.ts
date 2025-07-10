7b6648504bd0a69e741a1fd618ca94c6d6157b87
Ananda <ananda10@gmail.com>
2025-07-10 17:32:51 +0200

    fix: métier rome

--- server/src/common/repositories/romeMetier.ts
+++ server/src/common/repositories/romeMetier.ts
@@ -1,6 +1,12 @@
 import { SqlRepository } from "./base.js";
 import { kdb as defaultKdb } from "../db/db.js";
-import { DB } from "../db/schema.js";
+import { DB, RomeMetierState } from "../db/schema.js";
+import { DeleteResult, UpdateResult } from "kysely";
+
+export const ROME_METIER_STATE: { [k: string]: RomeMetierState } = {
+  updated: "updated",
+  update_waiting: "update_waiting",
+};
 
 export class RomeMetierRepository extends SqlRepository<DB, "romeMetier"> {
   constructor(kdb = defaultKdb) {
@@ -20,9 +26,26 @@ export class RomeMetierRepository extends SqlRepository<DB, "romeMetier"> {
         transitionDemographique: null,
         createdAt: null,
         updatedAt: null,
+        state: null,
       },
       kdb
     );
+  }
+
+  async startUpdate() {
+    const result = (await this.updateBy(
+      {
+        state: ROME_METIER_STATE.update_waiting,
+      },
+      {},
+      false
+    )) as UpdateResult[];
+    return result ? Number(result[0].numUpdatedRows) : 0;
+  }
+
+  async removeStale() {
+    const result = (await this.remove({ state: ROME_METIER_STATE.update_waiting }, false)) as DeleteResult[];
+    return result ? Number(result[0].numDeletedRows) : 0;
   }
 }
 
