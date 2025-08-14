437e1be5bf7fd5617ae6ece279a03eb7c4791ae1
Ananda <ananda10@gmail.com>
2025-08-14 11:39:33 +0200

    feat: matomo avec cookies

--- ui/app/components/ConsentManagement.tsx
+++ ui/app/components/ConsentManagement.tsx
@@ -3,14 +3,10 @@ import { useLocalStorage } from "usehooks-ts";
 import { useSearchParams } from "next/navigation";
 import { createConsentManagement } from "@codegouvfr/react-dsfr/consentManagement";
 import { useEffect, useMemo } from "react";
+import { UseConsent } from "@codegouvfr/react-dsfr/consentManagement/useConsent";
 
-export const {
-  ConsentBannerAndConsentManagement,
-  FooterConsentManagementItem,
-  FooterPersonalDataPolicyItem,
-  useConsent,
-} = {
-  ...createConsentManagement({
+const { ConsentBannerAndConsentManagement, FooterConsentManagementItem, FooterPersonalDataPolicyItem, useConsent } =
+  createConsentManagement({
     finalityDescription: ({ lang }) => ({
       analytics: {
         title: "Analyse",
@@ -21,28 +17,42 @@ export const {
       //   description: "Nous utilisons des cookies pour vous proposer des contenus adaptés à vos centres d’intérêts.",
       // },
     }),
-  }),
-  useConsent: () => {
-    // Override useConsent for now (experimentation)
-    const searchParams = useSearchParams();
-    const disableTrackingParams = searchParams.get("notracking") ? searchParams.get("notracking") === "true" : null;
-    const [disableTracking, saveDisableTracking] = useLocalStorage<boolean>(
-      "disableTracking",
-      disableTrackingParams !== null ? disableTrackingParams : false
-    );
-
-    const consent = useMemo(() => {
-      return {
-        finalityConsent: { analytics: !disableTracking },
-      };
-    }, [disableTracking]);
-
-    useEffect(() => {
-      if (disableTrackingParams !== null) {
-        saveDisableTracking(disableTrackingParams);
+    consentCallback: async ({ finalityConsent, finalityConsent_prev }) => {
+      if (finalityConsent_prev === undefined) {
+        location.reload();
       }
-    }, [saveDisableTracking, disableTrackingParams]);
+    },
+  });
 
-    return consent;
-  },
+const useConsentOverride = ((useConsent: UseConsent<"analytics">) => () => {
+  // Override useConsent for now (experimentation)
+  const searchParams = useSearchParams();
+  const disableTrackingParams = searchParams.get("notracking") ? searchParams.get("notracking") === "true" : null;
+  const [disableTracking, saveDisableTracking] = useLocalStorage<boolean>(
+    "disableTracking",
+    disableTrackingParams !== null ? disableTrackingParams : false
+  );
+
+  const { finalityConsent } = useConsent();
+
+  const consent = useMemo(() => {
+    return {
+      finalityConsent: { ...finalityConsent, analytics: finalityConsent?.analytics && !disableTracking },
+    };
+  }, [disableTracking, finalityConsent]);
+
+  useEffect(() => {
+    if (disableTrackingParams !== null) {
+      saveDisableTracking(disableTrackingParams);
+    }
+  }, [saveDisableTracking, disableTrackingParams]);
+
+  return consent;
+})(useConsent);
+
+export {
+  ConsentBannerAndConsentManagement,
+  FooterConsentManagementItem,
+  FooterPersonalDataPolicyItem,
+  useConsentOverride as useConsent,
 };
