128686d60fb33a40ae44922b72b1c917d189500e
Ananda <ananda10@gmail.com>
2025-08-28 13:15:34 +0200

    chore: separate build and deploy

--- .infra/scripts/run-job.sh
+++ .infra/scripts/run-job.sh
@@ -6,18 +6,19 @@ readonly INFRA_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/.."
 readonly VAULT_DIR="${INFRA_DIR}/vault"
 readonly VAULT_FILE="${VAULT_DIR}/vault.yml"
 readonly ANSIBLE_DIR="${INFRA_DIR}/ansible"
-readonly APP_VERSION=${1:?"Merci de préciser une version (correspondra au tag des images docker)"}
-readonly APP_NAMESPACE=${2:?"Merci de préciser un namespace (ex. recette, production ou nom de la branche)"}
+readonly ENV_FILTER=${1:?"Merci de préciser un environnement (ex. dev, recette ou production)"}
+readonly APP_VERSION=${2:?"Merci de préciser une version (tag de l'image docker)"}
 readonly JOB_ARGS=${3:?"Merci de préciser un job (en json, ex : '[\"importAll\", \"-j\", \"test\"]'"}
 readonly VAULT_PASSWORD_FILE=${VAULT_PASSWORD_FILE:="${INFRA_DIR}/scripts/vault/get-vault-password-client.sh"}
+
 shift 3
 
-echo "Lancement d'un job pour le déploiement ${APP_NAMESPACE}..."
+echo "Lancement d'un job pour le déploiement ${APP_VERSION}..."
 ansible-galaxy collection install community.docker
 ansible-galaxy collection install kubernetes.core
-ansible-playbook -e "APP_NAMESPACE=${APP_NAMESPACE}" -e "APP_VERSION=${APP_VERSION}" \
+ansible-playbook -e "ENV=${ENV_FILTER}" -e "APP_VERSION=${APP_VERSION}" \
     --extra-vars "@${VAULT_FILE}" \
     --vault-password-file="${VAULT_PASSWORD_FILE}" \
     -e "job_args='${JOB_ARGS}'" \
-    -i "${INFRA_DIR}/env.ini" --limit "local" \
+    -i "${INFRA_DIR}/env.ini" --limit "${ENV_FILTER}"  \
     "${ANSIBLE_DIR}/job.yml"
