06e43c4b345fe8316f3367391d0b579c1ab5f2ec
Ananda <ananda10@gmail.com>
2025-08-28 13:29:06 +0200

    chore: separate build and deploy

--- .infra/scripts/build-app.sh
+++ .infra/scripts/build-app.sh
@@ -10,7 +10,7 @@ readonly ENV_FILTER=${1:?"Merci de préciser un environnement (ex. dev, recette 
 readonly APP_VERSION=${2:?"Merci de préciser une version (utiliser pour le tag de l'image docker, le namespace k8s)"}
 readonly VAULT_PASSWORD_FILE=${VAULT_PASSWORD_FILE:="${INFRA_DIR}/scripts/vault/get-vault-password-client.sh"}
 
-shift 3
+shift 2
 
 echo "Création des images Docker pour ${APP_VERSION}..."
 ansible-galaxy collection install community.docker
