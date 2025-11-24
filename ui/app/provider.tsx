71461e67a2fde1ce761e9a1d91f77a79bdd045c7
Ananda <ananda10@gmail.com>
2025-11-24 16:22:02 +0100

    feat: ssr home et recherche

--- ui/app/provider.tsx
+++ ui/app/provider.tsx
@@ -2,11 +2,16 @@
 
 import React from "react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import RouterUpdaterProvider from "./(accompagnateur)/context/RouterUpdaterContext";
 
 function Providers({ children }: React.PropsWithChildren) {
   const [client] = React.useState(new QueryClient());
 
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={client}>
+      <RouterUpdaterProvider>{children}</RouterUpdaterProvider>
+    </QueryClientProvider>
+  );
 }
 
 export default Providers;
